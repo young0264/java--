c573189b59a7685c93476595febf6b627256b078
youngGit <ny2485@naver.com>
2022-05-13 13:48:25 +0900

    출력

---
+++ src/main/java/youngCalendar/Calendar.java
@@ -0,0 +1,15 @@
+package youngCalendar;
+
+public class Calendar {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calendar");
+        System.out.println(
+                "일 월 화 수 목 금 토 \n" +
+                        "--------------------\n" +
+                        "1  2  3  4  5  6  7 \n" +
+                        "8  9  10 11 12 13 14 \n" +
+                        "15 16 17 18 19 20 21 \n" +
+                        "22 23 24 25 26 27 28"
+        );
+    }
+}
