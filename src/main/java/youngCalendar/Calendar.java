3dee6e7a11202c70c4719d7dbf7ab45c3abb9f98
youngGit <ny2485@naver.com>
2022-05-14 14:03:52 +0900

    간단 덧셈

--- src/main/java/youngCalendar/Calendar.java
+++ src/main/java/youngCalendar/Calendar.java
@@ -1,5 +1,7 @@
 package youngCalendar;
 
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args) {
         System.out.println("Hello, Calendar");
@@ -11,5 +13,16 @@ public class Calendar {
                         "15 16 17 18 19 20 21 \n" +
                         "22 23 24 25 26 27 28"
         );
+
+        //월입력 -> 며칠
+        Scanner sc = new Scanner(System.in);
+        int[] arr_month = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int m = sc.nextInt();
+        System.out.println("달을 입력하세요.");
+        System.out.printf("%d월은 %d일까지 있습니다.\n", m, arr_month[m-1]);
+
     }
+
 }
+
+
